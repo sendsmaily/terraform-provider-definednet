2ee24b89a9b074e1adc51cfcede3613367948987
Janar Todesk <janar@smaily.com>
2024-10-17 10:40:27 +0300

    provider: Add initial provider implementation (#2)
    Set the provider up with Defined.net HTTP API client and add required moving parts for the documentation generator.

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -2,13 +2,23 @@ package provider
 
 import (
 	"context"
+	_ "embed"
 
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/provider"
 	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/sendsmaily/terraform-provider-definednet/internal/definednet"
 )
+
+const (
+	// DefinednetApiEndpoint declares the default Defined.net HTTP API endpoint.
+	DefinednetAPIEndpoint = "https://api.defined.net/"
+)
+
+//go:embed provider.md
+var providerDescription string
 
 // New creates a Defined.net Terraform provider.
 func New(version string) func() provider.Provider {
@@ -24,11 +34,11 @@ type Provider struct {
 	version string
 }
 
-var _ provider.Provider = &Provider{}
+var _ provider.Provider = (*Provider)(nil)
 
 // Configuration declares the provider's configuration options.
 type Configuration struct {
-	Endpoint types.String `tfsdk:"endpoint"`
+	Token types.String `tfsdk:"token"`
 }
 
 // Metadata returns the provider's metadata.
@@ -39,11 +49,31 @@ func (p *Provider) Metadata(ctx context.Context, req provider.MetadataRequest, r
 
 // Schema returns the provider's configuration schema.
 func (p *Provider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
-	resp.Schema = schema.Schema{}
+	resp.Schema = schema.Schema{
+		Description:         "Interact with Defined.net's managed Nebula control plane.",
+		MarkdownDescription: providerDescription,
+		Attributes: map[string]schema.Attribute{
+			"token": schema.StringAttribute{
+				Description: "Defined.net HTTP API token",
+				Required:    true,
+				Sensitive:   true,
+			},
+		},
+	}
 }
 
 // Configure configures the provider with user passed options.
 func (p *Provider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+	var config Configuration
+
+	resp.Diagnostics.Append(req.Config.Get(ctx, &config)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	client := definednet.NewClient(DefinednetAPIEndpoint, config.Token.String())
+	resp.ResourceData = client
+	resp.DataSourceData = client
 }
 
 // Resources returns a slice of resources available on the provider.
