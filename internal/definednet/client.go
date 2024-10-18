28a8608007601a72dd3f40f6fd49b848372abad8
Janar Todesk <janar@smaily.com>
2024-10-18 11:20:26 +0300

    Implement a general purpose Defined.net HTTP API client

--- internal/definednet/client.go
+++ internal/definednet/client.go
@@ -1,19 +1,99 @@
 package definednet
 
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/samber/lo"
+)
+
 // NewClient creates a Defined.net HTTP API client.
-func NewClient(endpoint, token string) Client {
-	return &client{
-		endpoint: endpoint,
-		token:    token,
+func NewClient(endpoint, token string) (Client, error) {
+	if lo.IsEmpty(strings.TrimSpace(endpoint)) {
+		return nil, errors.New("endpoint URL must be set")
 	}
+
+	endpointURL, err := url.ParseRequestURI(endpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	if lo.IsEmpty(strings.TrimSpace(token)) {
+		return nil, errors.New("authorization token must be set")
+	}
+
+	return &client{
+		endpoint: endpointURL,
+		token:    token,
+	}, nil
 }
 
 // Client is a Defined.net HTTP API client.
-type Client interface{}
+type Client interface {
+	Do(ctx context.Context, method string, path []string, request, response any) error
+}
 
 type client struct {
-	endpoint string
+	endpoint *url.URL
 	token    string
 }
 
-var _ Client = (*client)(nil)
+func (c *client) Do(ctx context.Context, method string, path []string, reqPayload, respPayload any) error {
+	var buf bytes.Buffer
+
+	if reqPayload != nil {
+		if err := json.NewEncoder(&buf).Encode(reqPayload); err != nil {
+			return fmt.Errorf("error encoding request payload: %w", err)
+		}
+	}
+
+	req, err := http.NewRequestWithContext(
+		ctx,
+		method,
+		c.endpoint.JoinPath(lo.Map(path, func(p string, _ int) string {
+			return url.PathEscape(p)
+		})...).String(),
+		&buf,
+	)
+
+	if err != nil {
+		return fmt.Errorf("error compiling HTTP request: %w", err)
+	}
+
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", c.token))
+	req.Header.Set("User-Agent", "Terraform-smaily-definednet/0.1.0")
+	if reqPayload != nil {
+		req.Header.Set("Content-Type", "application/json")
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("error executing HTTP request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 400 {
+		buf.Reset() // Let's recycle it.
+		if _, err := io.Copy(&buf, resp.Body); err != nil {
+			return fmt.Errorf("error reading error response: %w", err)
+		}
+
+		return fmt.Errorf("code=%d reason=%s", resp.StatusCode, buf.String())
+	}
+
+	if respPayload != nil {
+		if err := json.NewDecoder(resp.Body).Decode(respPayload); err != nil {
+			return fmt.Errorf("error decoding response payload: %w", err)
+		}
+	}
+
+	return nil
+}
---
+++ internal/definednet/client_test.go
@@ -0,0 +1,241 @@
+package definednet_test
+
+import (
+	"net/http"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/ghttp"
+	"github.com/onsi/gomega/gstruct"
+	"github.com/samber/lo"
+	"github.com/sendsmaily/terraform-provider-definednet/internal/definednet"
+)
+
+var _ = Describe("API client's invariants", func() {
+	Specify("client requires a valid endpoint URL", func() {
+		Expect(definednet.NewClient("http://localhost:8000/api/v1", "supersecret")).Error().
+			NotTo(HaveOccurred(), "assert sanity")
+
+		Expect(definednet.NewClient("", "supersecret")).Error().
+			To(MatchError("endpoint URL must be set"), "missing endpoint URL")
+
+		Expect(definednet.NewClient("  ", "supersecret")).Error().
+			To(MatchError("endpoint URL must be set"), "empty endpoint URL")
+
+		Expect(definednet.NewClient("invalid.url", "supersecret")).Error().
+			To(MatchError(`parse "invalid.url": invalid URI for request`), "invalid endpoint URL")
+	})
+
+	Specify("client requires a non-zero authorization token", func() {
+		Expect(definednet.NewClient("http://localhost:8000/api/v1", "supersecret")).Error().
+			NotTo(HaveOccurred(), "assert sanity")
+
+		Expect(definednet.NewClient("http://localhost:8000/api/v1", "")).Error().
+			To(MatchError("authorization token must be set"), "missing token")
+
+		Expect(definednet.NewClient("http://localhost:8000/api/v1", "  ")).Error().
+			To(MatchError("authorization token must be set"), "empty token")
+	})
+})
+
+var _ = Describe("executing API requests", func() {
+	var (
+		server *ghttp.Server
+		client definednet.Client
+	)
+
+	BeforeEach(func() {
+		server = ghttp.NewServer()
+		DeferCleanup(server.Close)
+
+		client = lo.Must(definednet.NewClient(server.URL(), "supersecret"))
+	})
+
+	Context("request headers", func() {
+		Specify("all requests are executed with common HTTP headers", func(ctx SpecContext) {
+			server.AppendHandlers(ghttp.RespondWith(http.StatusOK, nil))
+			Expect(client.Do(ctx, http.MethodGet, []string{}, nil, nil)).To(Succeed())
+
+			Expect(server.ReceivedRequests()).
+				To(HaveExactElements(
+					HaveField("Header", gstruct.MatchKeys(gstruct.IgnoreExtras, gstruct.Keys{
+						"Accept":        HaveExactElements("application/json"),
+						"Authorization": HaveExactElements("Bearer supersecret"),
+						"User-Agent":    HaveExactElements("Terraform-smaily-definednet/0.1.0"),
+					})),
+				))
+		})
+
+		When("request does not specify body payload", func() {
+			Specify("Content-Type header is not set on the request", func(ctx SpecContext) {
+				server.AppendHandlers(ghttp.RespondWith(http.StatusOK, nil))
+				Expect(client.Do(ctx, http.MethodPost, []string{}, nil, nil)).To(Succeed())
+
+				Expect(server.ReceivedRequests()).
+					To(HaveExactElements(HaveField("Header", Not(HaveKey("Content-Type")))))
+			})
+		})
+
+		When("request specifies body payload", func() {
+			Specify("Content-Type header is set on the request", func(ctx SpecContext) {
+				server.AppendHandlers(ghttp.RespondWith(http.StatusOK, nil))
+				Expect(client.Do(ctx, http.MethodPost, []string{}, map[string]any{}, nil)).To(Succeed())
+
+				Expect(server.ReceivedRequests()).
+					To(HaveExactElements(HaveField("Header",
+						HaveKeyWithValue("Content-Type", HaveExactElements("application/json")),
+					)))
+			})
+		})
+	})
+
+	Context("request path", func() {
+		Specify("paths are compiled from passed components", func(ctx SpecContext) {
+			components := []string{"path", "compiled", "from", "components"}
+			server.AppendHandlers(ghttp.VerifyRequest(http.MethodGet, "/path/compiled/from/components"))
+			Expect(client.Do(ctx, http.MethodGet, components, nil, nil)).To(Succeed())
+			Expect(server.ReceivedRequests()).NotTo(BeEmpty(), "assert sanity")
+		})
+
+		Specify("paths are protected from URL injection", func(ctx SpecContext) {
+			components := []string{"spaced value", "path/../../traversal/attempt"}
+			server.AppendHandlers(ghttp.VerifyRequest(http.MethodGet, "/spaced value/path/../../traversal/attempt"))
+			Expect(client.Do(ctx, http.MethodGet, components, nil, nil)).To(Succeed())
+			Expect(server.ReceivedRequests()).NotTo(BeEmpty(), "assert sanity")
+		})
+	})
+
+	Context("request body", func() {
+		type (
+			nested struct {
+				Field string `json:"field"`
+			}
+
+			request struct {
+				Field  string `json:"field"`
+				Nested nested `json:"nested"`
+			}
+		)
+
+		Specify("request payload containers are JSON-encoded into HTTP body", func(ctx SpecContext) {
+			server.AppendHandlers(ghttp.CombineHandlers(
+				ghttp.VerifyRequest(http.MethodPost, "/"),
+				ghttp.VerifyJSON(`{"field":"value","nested":{"field":"nested_value"}}`),
+				ghttp.RespondWith(http.StatusOK, nil),
+			))
+
+			Expect(client.Do(
+				ctx,
+				http.MethodPost,
+				[]string{},
+				request{
+					Field: "value",
+					Nested: nested{
+						Field: "nested_value",
+					},
+				}, nil,
+			)).To(Succeed())
+
+			Expect(server.ReceivedRequests()).NotTo(BeEmpty(), "assert sanity")
+		})
+	})
+})
+
+var _ = Describe("handling HTTP API success responses", func() {
+	type response struct {
+		Field  string `json:"field"`
+		Nested struct {
+			Field string `json:"field"`
+		} `json:"nested"`
+	}
+
+	var (
+		server *ghttp.Server
+		client definednet.Client
+	)
+
+	BeforeEach(func() {
+		server = ghttp.NewServer()
+		DeferCleanup(server.Close)
+
+		client = lo.Must(definednet.NewClient(server.URL(), "supersecret"))
+	})
+
+	Specify("API responses are deserialized into passed response payload container", func(ctx SpecContext) {
+		var container response
+
+		server.AppendHandlers(ghttp.RespondWithJSONEncoded(http.StatusOK, map[string]any{
+			"field": "value",
+			"nested": map[string]any{
+				"field": "nested_value",
+			},
+		}))
+
+		Expect(client.Do(ctx, http.MethodGet, []string{}, nil, &container)).To(Succeed())
+		Expect(container).To(gstruct.MatchAllFields(gstruct.Fields{
+			"Field": Equal("value"),
+			"Nested": gstruct.MatchAllFields(gstruct.Fields{
+				"Field": Equal("nested_value"),
+			}),
+		}))
+	})
+
+	Specify("unexpected keys in responses are ignored", func(ctx SpecContext) {
+		var container response
+
+		server.AppendHandlers(ghttp.RespondWithJSONEncoded(http.StatusOK, map[string]any{
+			"field": "value",
+			"nested": map[string]any{
+				"field": "nested_value",
+			},
+			"unexpected": "field",
+		}))
+
+		Expect(client.Do(ctx, http.MethodGet, []string{}, nil, &container)).To(Succeed())
+		Expect(container).To(gstruct.MatchAllFields(gstruct.Fields{
+			"Field": Equal("value"),
+			"Nested": gstruct.MatchAllFields(gstruct.Fields{
+				"Field": Equal("nested_value"),
+			}),
+		}), "assert sanity")
+	})
+
+	When("response payload container is not provided", func() {
+		Specify("response payload decoding is skipped", func(ctx SpecContext) {
+			server.AppendHandlers(ghttp.RespondWithJSONEncoded(http.StatusOK, map[string]any{
+				"field": "value",
+				"nested": map[string]any{
+					"field": "nested_value",
+				},
+			}))
+
+			Expect(client.Do(ctx, http.MethodGet, []string{}, nil, nil)).To(Succeed())
+		})
+	})
+})
+
+var _ = Describe("handling HTTP API error responses", func() {
+	var (
+		server *ghttp.Server
+		client definednet.Client
+	)
+
+	BeforeEach(func() {
+		server = ghttp.NewServer()
+		DeferCleanup(server.Close)
+
+		client = lo.Must(definednet.NewClient(server.URL(), "supersecret"))
+	})
+
+	Specify("API errors are reported to the user", func(ctx SpecContext) {
+		server.AppendHandlers(ghttp.RespondWithJSONEncoded(
+			http.StatusBadRequest,
+			map[string]any{
+				"server": "response",
+			},
+		))
+
+		Expect(client.Do(ctx, http.MethodGet, []string{}, nil, nil)).
+			To(MatchError(`code=400 reason={"server":"response"}`))
+	})
+})
