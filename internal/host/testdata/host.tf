8db436abde37370d16d9f53c9d6482e5a5a54571
Janar Todesk <janar@smaily.com>
2024-10-23 10:42:52 +0300

    resource/host: Parameterize test data specification (#19)
    Reduce coupling in the acceptance test layer by reusing the test data specification in different contexts.

--- internal/host/testdata/host.tf
+++ internal/host/testdata/host.tf
@@ -2,9 +2,25 @@ provider "definednet" {
   token = "supersecret"
 }
 
+variable "name" {
+  type = string
+}
+
+variable "network_id" {
+  type = string
+}
+
+variable "role_id" {
+  type = string
+}
+
+variable "tags" {
+  type = list(string)
+}
+
 resource "definednet_host" "test" {
-  name       = "host.defined.test"
-  network_id = "network-id"
-  role_id    = "role-id"
-  tags       = ["tag:one", "tag:two"]
+  name       = var.name
+  network_id = var.network_id
+  role_id    = var.role_id
+  tags       = var.tags
 }
--- internal/host/testdata/import.tf
+++
@@ -1,8 +0,0 @@
-provider "definednet" {
-  token = "supersecret"
-}
-
-resource "definednet_host" "test" {
-  name       = "host.defined.test"
-  network_id = "network-id"
-}
