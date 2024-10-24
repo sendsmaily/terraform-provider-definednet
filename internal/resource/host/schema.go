ebbffadbd18b2add6d7bd372d76c28a8bd35c47c
Janar Todesk <janar@smaily.com>
2024-10-24 08:25:14 +0300

    resource: Implement schema validation rules
    * Validate `name` attribute on `host.Schema` and `lighthouse.Schema` conforms to a maximum length constraint.
    * Validate `tags` attribute on `host.Schema` and `lighthouse.Schema` conforms to the expected format constraint.
    * Validate `static_addresses` attribute contains a list of IP addresses on `lighthouse.Schema`.

--- internal/resource/host/schema.go
+++ internal/resource/host/schema.go
@@ -3,26 +3,26 @@ package host
 import (
 	_ "embed"
 
+	"github.com/hashicorp/terraform-plugin-framework-validators/listvalidator"
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/sendsmaily/terraform-provider-definednet/internal/validation"
 )
 
 // Schema is the host resource's schema.
 var Schema = schema.Schema{
 	MarkdownDescription: resourceDescription,
 	Attributes: map[string]schema.Attribute{
-		"id": schema.StringAttribute{
-			Description: "Host's ID",
-			Computed:    true,
-			PlanModifiers: []planmodifier.String{
-				stringplanmodifier.UseStateForUnknown(),
-			},
-		},
 		"name": schema.StringAttribute{
 			Description: "Host's name",
 			Required:    true,
+			Validators: []validator.String{
+				stringvalidator.LengthAtMost(255),
+			},
 		},
 		"network_id": schema.StringAttribute{
 			Description: "Enrolled Network ID",
@@ -35,17 +35,28 @@ var Schema = schema.Schema{
 			Description: "Host's role ID on Defined.net",
 			Optional:    true,
 		},
+		"tags": schema.ListAttribute{
+			Description: "Host's tags on Defined.net",
+			ElementType: types.StringType,
+			Optional:    true,
+			Validators: []validator.List{
+				listvalidator.UniqueValues(),
+				listvalidator.ValueStringsAre(validation.HostTag()),
+			},
+		},
+		"id": schema.StringAttribute{
+			Description: "Host's ID",
+			Computed:    true,
+			PlanModifiers: []planmodifier.String{
+				stringplanmodifier.UseStateForUnknown(),
+			},
+		},
 		"ip_address": schema.StringAttribute{
 			Description: "Host's IP address on Defined.net overlay network",
 			Computed:    true,
 			PlanModifiers: []planmodifier.String{
 				stringplanmodifier.UseStateForUnknown(),
 			},
-		},
-		"tags": schema.ListAttribute{
-			Description: "Host's tags on Defined.net",
-			ElementType: types.StringType,
-			Optional:    true,
 		},
 		"enrollment_code": schema.StringAttribute{
 			Description: "Host's enrollment code",
