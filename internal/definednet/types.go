4bafd60cca4f673cfce3b7dcc9b9cb887a82db20
Janar Todesk <janar@smaily.com>
2024-11-28 06:55:38 +0200

    Declare `ConfigOverride` data model type

---
+++ internal/definednet/types.go
@@ -0,0 +1,7 @@
+package definednet
+
+// ConfigOverride is a data model for Defined.net host configuration override.
+type ConfigOverride struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
